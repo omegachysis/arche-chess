1f02b9c705e0cd4a204a9c93105807cd71d5135a
Matthew Robinson <omegachysis@outlook.com>
2014-02-09 16:50:26 -0500

    Created test screen

--- arche-chess/Main.py
+++ arche-chess/Main.py
@@ -12,7 +12,16 @@ def main():
 
     game = engine.Game(1024, 768, False)
 
+    game.startApp(MainMenu())
+
     game.run()
+
+class MainMenu(engine.Application):
+    def __init__(self):
+        super(MainMenu, self).__init__()
+
+        self.backgroundColor = (255,255,255)
+
 
 if __name__ == "__main__":
     debug.test(main)
