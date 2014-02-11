bd7c9088b36ba178da6979feda905f8042fb7aa9
Matthew Robinson <omegachysis@outlook.com>
2014-02-10 22:56:57 -0500

    Created chess players

--- arche-chess/chess.py
+++ arche-chess/chess.py
@@ -9,7 +9,8 @@ log = debug.log("game chess")
 
 def main():
     game = engine.Game(1024, 768, False)
-    game.startApp(Chess())
+    game.startApp(Chess(PlayerHuman(), PlayerComputer()))
+    game.app.gameStart()
     game.run()
 
 class Player(object):
@@ -19,11 +20,11 @@ class PlayerHuman(Player):
     def __init__(self):
         pass
     
-    def move(self, board):
+    def move(self):
         move = None
         while True:
             move = input("Your move: ")
-            if board.isMoveLegal(move):
+            if self.app.board.isMoveLegal(move):
                 break
             else:
                 print("Not a legal move!")
@@ -33,7 +34,7 @@ class PlayerComputer(Player):
     def __init__(self, depth=12):
         self.depth = depth
 
-    def move(self, board):
+    def move(self):
         move = None
         self.app.enginePut("go depth {}".format(self.depth))
         while True:
@@ -43,21 +44,34 @@ class PlayerComputer(Player):
                 move = results[0]
                 pondermove = results[2]
                 break
-        board.move(move)
+        self.app.board.move(move)
 
 class Board(object):
     moves = []
     app = None
     def __init__(self):
         pass
+    
     def move(self, move):
         moves.append(move)
+        
     def isMoveLegal(self, move):
-        pass
+        self.app.enginePut("go depth 2 searchmoves {}".format(move))
+        while True:
+            r = self.app.engineGet()
+            if "bestmove" in r:
+                results = r[r.index("bestmove") + 9:].split(" ")
+                testmove = results[0]
+                pondermove = results[2]
+                break
+        return testmove != "(none)"
+    
+
 
 class Chess(engine.Application):
     playerWhite = None
     playerBlack = None
+    board = None
     def __init__(self, playerWhite, playerBlack):
         super(Chess, self).__init__()
 
@@ -66,7 +80,7 @@ class Chess(engine.Application):
         self.playerWhite = playerWhite
         self.playerBlack = playerBlack
 
-        log.info("Starting stockfish")
+        log.debug("starting stockfish")
         self.engine = subprocess.Popen(
             "chess/stockfish-dd-64-modern.exe",
             universal_newlines = True,
@@ -81,6 +95,7 @@ class Chess(engine.Application):
 
     def enginePut(self, command):
         self.engine.stdin.write(command + "\n")
+        
     def engineGet(self):
         data = ""
         engine.stdin.write('isready\n')
@@ -92,8 +107,18 @@ class Chess(engine.Application):
                 data += text
         return data
 
+    def syncPosition(self):
+        self.enginePut("position startpos moves {}".format(' '.join(self.board.moves)))
+
     def gameStart(self):
-        pass
+        self.board = Board()
+        self.enginePut("ucinewgame")
+
+        while True:
+            self.syncPosition()
+            self.playerWhite.move()
+            self.syncPosition()
+            self.playerBlack.move()
 
 if __name__ == "__main__":
     debug.test(main)
